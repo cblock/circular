0db65f95af1054ff4ea7ea2c8be93bf3e0bfd87e
CB <cb@CBs-MacBook-Pro.fritz.box>
2020-09-13 14:27:36 +0200

    Fix formatting for mix.exs

--- mix.exs
+++ mix.exs
@@ -13,7 +13,12 @@ defmodule Circular.MixProject do
       deps: deps(),
       dialyzer: [plt_add_deps: :app_tree],
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
